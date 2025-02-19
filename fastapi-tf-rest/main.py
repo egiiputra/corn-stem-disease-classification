b9670ce1c67738ec70060db3d1bb0d58dba8712b
egiiputra <egiputra450@gmail.com>
2025-02-19 21:52:11 +0700

    feat: read and preprocessing image
    read image from body request and preprocessing (resize to the approriate
    size, and save into batch image tensor)

--- fastapi-tf-rest/main.py
+++ fastapi-tf-rest/main.py
@@ -6,6 +6,7 @@ from fastapi import (
     Response, 
     status
 )
+import tensorflow as tf
 
 app = FastAPI()
 
@@ -14,15 +15,31 @@ def read_root():
     return "Hello world!"
 
 @app.post("/predicts")
-def predicts(images: List[UploadFile], response: Response):
+async def predicts(images: List[UploadFile], response: Response):
 
     # Check MIME Type make sure that file is image
     for image in images:
         if image.content_type not in ('image/jpeg', 'image/png'):
             response.status_code = status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
             return { 'message' : 'Just can receive image file type'}
+    
+    # === GET INPUT ===
+    input_tensors = []
 
-    # TODO: Get image data in body request
+    for i, image in enumerate(images):
+        # Get image data from body request
+        tmp = tf.io.decode_image(
+            await image.read(),
+            channels=3,
+            dtype=tf.dtypes.uint8,
+            expand_animations=False
+        )
+
+        tmp = tf.image.resize(tmp, size=(224, 224), method='nearest')
+        input_tensors.append(tmp)
+
+    input_tensors = tf.convert_to_tensor(input_tensors, dtype=tf.float32)
+
     # TODO: Preprocessing image into approriate format and size
     # TODO: pass preprocessing result into model
     # TODO: return json (key: result, val: prediction probability)
